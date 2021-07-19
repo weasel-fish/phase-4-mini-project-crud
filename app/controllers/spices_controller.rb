eb1a5eb5dfaf2d340c8ffeaacbadd2030da70bcb
github username <github email address>
2021-07-19 12:14:22 -0400

    commit tree e3693fbbded8d117ba1b416b90974d1deabb4ea0

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,33 @@
+class SpicesController < ApplicationController
+    def index
+        spices = Spice.all
+        render json: spices
+    end
+
+    def create
+        spice = Spice.create(spice_params)
+        render json: spice, status: :created
+    end
+
+    def update
+        spice = find_spice
+        spice.update(spice_params)
+        render json: spice
+    end
+
+    def destroy
+        spice = find_spice
+        spice.destroy
+        head :no_content
+    end
+
+    private
+
+    def find_spice
+        Spice.find(params[:id])
+    end
+
+    def spice_params
+        params.permit(:title, :image, :description, :notes, :rating)
+    end
+end
